76b64955f4cc399ffab3efded439bb8ff19ef097
AThevon <adrienthevon@gmail.com>
2024-11-21 21:32:38 +0100

    modified cors to domains name

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
   'allowed_methods' => ['*'],
 
-  'allowed_origins' => ['*'],
+  'allowed_origins' => ['https://victordenay.com', 'https://www.victordenay.com', 'http://localhost:3000'],
 
   'allowed_origins_patterns' => [],
 
